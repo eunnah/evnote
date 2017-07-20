adfc2dc2198e56c85c25dbe209ec51c1419f2b9b
eunnah <eunnah@gmail.com>
2017-07-20 11:56:07 -0700

    fixed typo

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Route, Redirect } from 'react-router';
+import { Link, Route, Redirect } from 'react-router-dom';
 
 const sessionLinks = () => (
     <Redirect to="/login" />
